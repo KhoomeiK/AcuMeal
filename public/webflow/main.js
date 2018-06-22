185b5e13d758bf95acd3520ceecaf1859c876628
Rohan Pandey <rohan@Rohans-MacBook-Pro.local>
2018-06-22 15:45:07 -0700

    webflow

--- public/webflow/main.js
+++ public/webflow/main.js
@@ -17,7 +17,7 @@ $(() =>  {
             var user = result.user;
             myDoc = db.collection("users").doc(user.uid);
             myDoc.set({"Name": user.displayName}).then( () => {
-                window.location.replace("survey.html?" + user.uid);
+                window.location.replace("../survey.html?" + user.uid);
             });
         });
     }
