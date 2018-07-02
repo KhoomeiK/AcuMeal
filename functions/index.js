04eedd09315ec50929b379d4c11063579189a987
pointfour <lesha.seletskiy@gmail.com>
2018-07-02 16:14:23 -0700

    example

--- functions/index.js
+++ functions/index.js
@@ -5,50 +5,51 @@ const DecisionTree = require('decision-tree');
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.analyze = functions.firestore.document("users/{uid}")
-.onWrite((change, context) => {
-  const doc = change.data(); // gets user document that was written
-  // write your algorithm here using doc
+    .onWrite((change, context) => {
+        const doc = change.data(); // gets user document that was written
+        // write your algorithm here using doc
 
-  var training_data = [
-    {"color": "blue", "shape": "square", "liked":false },
-    {"color": "red", "shape": "square", "liked":false}, 
-    {"color":"blue", "shape":"circle", "liked":true},
-    {"color":"red", "shape":"circle", "liked":true},
-    {"color":"blue", "shape":"hexagon", "liked":false},
-    {"color":"red", "shape":"hexagon", "liked":false},
-    {"color":"yellow", "shape":"hexagon", "liked":true},
-    {"color":"yellow", "shape":"circle", "liked":true}
-  ];
+        var training_data = [
+            {"color": "blue", "shape": "square", "liked": false},
+            {"color": "red", "shape": "square", "liked": false},
+            {"color": "blue", "shape": "circle", "liked": true},
+            {"color": "red", "shape": "circle", "liked": true},
+            {"color": "blue", "shape": "hexagon", "liked": false},
+            {"color": "red", "shape": "hexagon", "liked": false},
+            {"color": "yellow", "shape": "hexagon", "liked": true},
+            {"color": "yellow", "shape": "circle", "liked": true}
+        ];
 
-  var test_data = [
-    {"color":"blue", "shape":"hexagon", "liked":false},
-    {"color":"red", "shape":"hexagon", "liked":false},
-    {"color":"yellow", "shape":"hexagon", "liked":true},
-    {"color":"yellow", "shape":"circle", "liked":true}
-  ]
-  var class_name = "liked";
+        var test_data = [
+            {"color": "blue", "shape": "hexagon", "liked": false},
+            {"color": "red", "shape": "hexagon", "liked": false},
+            {"color": "yellow", "shape": "hexagon", "liked": true},
+            {"color": "yellow", "shape": "circle", "liked": true}
+        ]
+        var class_name = "liked";
 
-  var features = ["color", "shape"];
+        var features = ["color", "shape"];
 
-  var dt = new DecisionTree(training_data, class_name, features);
+        var dt = new DecisionTree(training_data, class_name, features);
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+        exports.helloWorld = functions.https.onRequest((request, response) => {
+            response.send("Hello from Firebase!");
 
-  var predicted_class = dt.predict({
-    color: "blue",
-    shape: "hexagon"
-  });
+            var predicted_class = dt.predict({
+                color: "blue",
+                shape: "hexagon"
+            });
 
-  var accuracy = dt.evaluate(test_data);
+            var accuracy = dt.evaluate(test_data);
 
-  var treeModel = dt.toJSON();
-  // decision tree stuff
+            var treeModel = dt.toJSON();
+            // decision tree stuff
 
-  var mealPlan = "";
+            var mealPlan = "";
 
-  return change.after.ref.update({
-    mealPlan: mealPlan // set some mealPlan variable to whatever meal plan the algorithm created
-  }, {merge: true});
+            return change.after.ref.update({
+                mealPlan: mealPlan // set some mealPlan variable to whatever meal plan the algorithm created
+            }, {merge: true});
 
-})});
+        })
+    });
