a49c263993548175a2bd0fd6315635b45b857078
pointfour <lesha.seletskiy@gmail.com>
2018-07-05 01:49:48 -0700

    why wont this ever work reeee

--- public/script/survey.js
+++ public/script/survey.js
@@ -1,47 +1,3 @@
-let button;
-let done = false;
-setInterval(function () {
-    if (button == null) {
-        button = document.getElementsByClassName("stripe-button-el")[0];
-    } else if (!done) {
-        button.addEventListener("click", function () {
-            if (find("email") == "") {
-                return;
-            }
-            db.collection("ripyu").doc((new Date()).toString()).set({
-                name: find("fname") + " " + find("lname"),
-                email: find("email"),
-                age: find("age"),
-                height: find("height"),
-                weight: find("weight"),
-                gender: find("gender"),
-                disease: find("healthcondition"),
-                activity: find("activity"),
-                workout: find("workout"),
-                goal: find("goals"),
-                cooktime: find("cooktime"),
-                meal_count: find("meals"),
-                fruitsDisliked: multiplefind("fruitdislike"),
-                proteinPrefered: multiplefind("protein"),
-                carbsPrefered: multiplefind("carbs"),
-                flavourPreferd: multiplefind("flavour"),
-                cuisine: multiplefind("cuisine"),
-                allergies: multiplefind("allergies"),
-                healthinessOfMeal: find("health"),
-                wakeuptime: find("wakeuptime"),
-                sleeptime: find("sleeptime")
-
-            }).then(function () {
-                console.log("Document successfully written!");
-                document.getElementById("congratulations").innerText = "Congratulations"
-            }).catch(function (error) {
-                console.error("Error writing document: ", error);
-            })
-        });
-        done = true;
-    }
-}, 100);
-
 function find(thing) {
     return document.getElementById("survey").elements[thing].value;
 }
@@ -55,6 +11,42 @@ function multiplefind(index) {
         }
     }
     return ret;
+}
+
+
+function science() {
+    if (find("email") === "") {
+        return;
+    }
+    db.collection("ripyu").doc((new Date()).toString()).set({
+        name: find("fname") + " " + find("lname"),
+        email: find("email"),
+        age: find("age"),
+        height: find("height"),
+        weight: find("weight"),
+        gender: find("gender"),
+        disease: find("healthcondition"),
+        activity: find("activity"),
+        workout: find("workout"),
+        goal: find("goals"),
+        cooktime: find("cooktime"),
+        meal_count: find("meals"),
+        fruitsDisliked: multiplefind("fruitdislike"),
+        proteinPrefered: multiplefind("protein"),
+        carbsPrefered: multiplefind("carbs"),
+        flavourPreferd: multiplefind("flavour"),
+        cuisine: multiplefind("cuisine"),
+        allergies: multiplefind("allergies"),
+        healthinessOfMeal: find("health"),
+        wakeuptime: find("wakeuptime"),
+        sleeptime: find("sleeptime")
+
+    }).then(function () {
+        console.log("Document successfully written!");
+        document.getElementById("congratulations").innerText = "Congratulations"
+    }).catch(function (error) {
+        console.error("Error writing document: ", error);
+    })
 }
 
 //why doesn't this work
